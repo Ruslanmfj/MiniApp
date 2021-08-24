64d12e21eed9ab11117147367f13d82f3e657ca6
ivashin_rv <ivashin_rv@CP495PC>
2021-08-24 15:19:52 +0300

    Добавьте файлы проекта.

---
+++ MiniApp/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace MiniApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Loadandrun = new System.Windows.Forms.Button();
+            this.Returned = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Loadandrun
+            // 
+            this.Loadandrun.Location = new System.Drawing.Point(12, 12);
+            this.Loadandrun.Name = "Loadandrun";
+            this.Loadandrun.Size = new System.Drawing.Size(181, 61);
+            this.Loadandrun.TabIndex = 0;
+            this.Loadandrun.Text = "Загруить и выполнить";
+            this.Loadandrun.UseVisualStyleBackColor = true;
+            this.Loadandrun.Click += new System.EventHandler(this.Loadandrun_Click);
+            // 
+            // Returned
+            // 
+            this.Returned.Location = new System.Drawing.Point(12, 79);
+            this.Returned.Name = "Returned";
+            this.Returned.Size = new System.Drawing.Size(181, 209);
+            this.Returned.TabIndex = 1;
+            this.Returned.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 300);
+            this.Controls.Add(this.Returned);
+            this.Controls.Add(this.Loadandrun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Loadandrun;
+        private System.Windows.Forms.RichTextBox Returned;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ MiniApp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MiniApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Loadandrun_Click(object sender, EventArgs e)
+        {
+            int PC = 0;
+            int NB = 0;
+            int Other = 0;
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(openFileDialog1.FileName,System.Text.Encoding.UTF8,false))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        line = line.Remove(7, line.Length - 7);
+                        string line2 = line.Remove(0, 5);
+                        if (line2 == "PC") PC++;
+                        else if (line2 == "NB") NB++;
+                        else
+                        {
+                            Other++;
+                            Returned.Text = Returned.Text + "\n" + line;
+                        }
+                    }
+                }
+                Returned.Text = Returned.Text + "\n" + "Компьютеры: "+PC.ToString();
+                Returned.Text = Returned.Text + "\n" + "Ноутбуки: "+NB.ToString();
+                Returned.Text = Returned.Text + "\n" + "Другие: "+Other.ToString();
+                Returned.Text = Returned.Text + "\n" + "Всего: "+(PC+NB+Other).ToString();
+            }
+        }
+    }
+}
